11cc07f85519634eec09579b4e34a9bca1e45350
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-10-06 15:18:05 -0300

    feat(JS) use const to declare variables
    Chap: Boas praticas com variaveis

---
+++ 2-trabalhandoComVariáveis.js
@@ -0,0 +1,11 @@
+console.log("trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 29;
+const nome = ("Ricardo")
+
+console.log(nome);
+console.log(idade);
+
+//declarar variáveis usando const
+//não fazer: mes = "janeiro"
