f0ed92ad54091b19bfbc35ae55704cc8073a5515
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-10-07 13:43:26 -0300

    feat(js) add chained conditionals
    Chap: Ifs encadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,21 +6,28 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 17
+const idadeComprador = 17;
 console.log("Destinos Possíveis:");
 console.log(listaDeDestinos);
+
+const estaAcompanhada = true;
 
 if(idadeComprador >= 18){
     console.log("Comprador maior de idade");    
     listaDeDestinos.splice(1,1);  //remove item
+}else if(estaAcompanhada) {
+    console.log("O comprador é menor, mas está acompanhado");
+    listaDeDestinos.splice(1,1);  //remove item
 }else{
-    console.log("Não é maior de idade, não posso vender")
+    console.log("Não é maior de idade, não posso vender");
 }
+   
+
 
 console.log(listaDeDestinos);
 
-console.log(idadeComprador > 18);
-console.log(idadeComprador < 18);
-console.log(idadeComprador <= 18);
-console.log(idadeComprador >= 18);
-console.log(idadeComprador == 18);
+//console.log(idadeComprador > 18);
+//console.log(idadeComprador < 18);
+//console.log(idadeComprador <= 18);
+//console.log(idadeComprador >= 18);
+//console.log(idadeComprador == 18);
