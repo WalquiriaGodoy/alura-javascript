8f1d1777073b82cb283f44ffeed1bf18dfd2d266
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-10-08 18:55:57 -0300

    feat(method): add method to a class
    Chap: Comportamentos de Classes

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 class Cliente {
     nome;
     cpf;
+}
+class ContaCorrente {
     agencia;
     saldo;
-    rg;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
-cliente1 = new Cliente();
-cliente2 = new Cliente();
-
+const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-cliente1.rg = 123456789
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2)
+const ContaCorrenteRicardo = new ContaCorrente();
+ContaCorrenteRicardo.saldo = 0;
+ContaCorrenteRicardo.agencia = 1001;
+
+console.log(ContaCorrenteRicardo.saldo);
+
+ContaCorrenteRicardo.saldo = 100;
+console.log(ContaCorrenteRicardo.saldo);
+
+ContaCorrenteRicardo.sacar(200);
+
+console.log(ContaCorrenteRicardo.saldo);
+console.log(cliente1);
+console.log(cliente2);
