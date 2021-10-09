456e40b4f0ad03ac60e4e4909687c306ee5d38f1
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-10-09 19:00:41 -0300

    feat(set&get) add set and get to ContaCorrente class
    Chap: Getters e Setters

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
 const ContaCorrenteRicardo = new ContaCorrente();
 ContaCorrenteRicardo.agencia = 1001;
 ContaCorrenteRicardo.cliente = cliente1;
@@ -12,11 +16,10 @@ ContaCorrenteRicardo.depositar(500)
 
 const conta2 = new ContaCorrente();
 conta2.agencia = 102;
-conta2.cliente = null;
-conta2.cliente.nome = "Alice";
-conta2.cliente.cpf = 88822233309;
+conta2.cliente = cliente1;
 
 let valor = 200
 ContaCorrenteRicardo.transferir(valor, conta2);
 
-console.log(conta2);
+
+console.log(conta2.saldo);
