9f56c23997669fe41a2a293e981f128bb6ff62e5
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-10-09 14:28:08 -0300

    feat(class): add modules and class composition
    Chap: Composiçao de classes

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-class Cliente {
-    nome;
-    cpf;
-}
-class ContaCorrente {
-    agencia;
-    _saldo = 0; //#saldo possível campo privado
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if (valor <= 0){
-            return;
-        }
-        this._saldo += valor;
-    }
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -31,13 +11,13 @@ cliente2.cpf = 88822233309;
 
 const ContaCorrenteRicardo = new ContaCorrente();
 ContaCorrenteRicardo.agencia = 1001;
+ContaCorrenteRicardo.cliente = cliente1;
+ContaCorrenteRicardo.depositar(500)
 
-ContaCorrenteRicardo.depositar(-100);
-ContaCorrenteRicardo.depositar(100);
-ContaCorrenteRicardo.depositar(100);
+const conta2 = new ContaCorrente();
+conta2.agencia = 102;
+conta2.cliente = cliente2;
+ContaCorrenteRicardo.transferir(200, conta2);
 
-const valorSacado = ContaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
-
-
+console.log(conta2);
 console.log(ContaCorrenteRicardo);
