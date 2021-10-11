e7e01d4c0f945681e78fa3682da29092b646f368
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-10-11 14:10:48 -0300

    feat(inheritance) add two extensions of the account class
    Chap: Herança

--- modulo2/Conta.js
+++ modulo2/Conta.js
@@ -2,19 +2,32 @@ export class Conta {
     constructor(saldoInicial,  cliente, agencia){
         this._saldo = saldoInicial;
         this._cliente = cliente;
-        this.agencia = agencia;
+        this._agencia = agencia;
     }
+
+    set cliente(novoValor){
+        if (novoValor instanceof Cliente)
+        this._cliente = novoValor;
+    }
+    get cliente(){
+        return this._cliente;
+    }
+
+    
+    get saldo(){
+        return this._saldo;
+    }
+
     sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
+        let taxa = 1
+        const valorSacado = taxa*valor
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado;
             return valor;
         }
     }
 
     depositar(valor){
-        if (valor <= 0){
-            return;
-        }
         this._saldo += valor;
     }
     transferir(valor, conta) {
--- modulo2/index.js
+++ modulo2/index.js
@@ -1,14 +1,13 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
-import {ContaPoupanca} from "./ContaPoupanca.js";
-import {Conta} from "./Conta.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const ContaCorrenteRicardo = new Conta(0, cliente1, 1001);
+const ContaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 ContaCorrenteRicardo.depositar(500);
 ContaCorrenteRicardo.sacar(100);
 
-const contaPoupanca = new Conta(50, cliente1, 1001)
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
 console.log(contaPoupanca);
 console.log(ContaCorrenteRicardo);
