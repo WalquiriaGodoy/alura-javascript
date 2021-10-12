1adb58bb7ba1ced900fb4d4a66697dcb60c5f7c7
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-10-11 22:25:44 -0300

    feat(authentication): add authentication system
    Chap: Polimorfismo

--- modulo2/index.js
+++ modulo2/index.js
@@ -1,16 +1,13 @@
 import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./Contas/ContaCorrente.js";
-import { ContaPoupanca } from "./Contas/ContaPoupanca.js";
-import {ContaSalario} from "./Contas/ContaSalario.js";
+import {Gerente} from "./Funcionarios/Gerente.js";
+import {Diretor} from "./Funcionarios/Diretor.js";
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456");
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
+const gerente = new Gerente("Ricardo", 5000, 12345678901);
+gerente.cadastrarSenha("123");
 
-console.log(contaPoupanca);
-console.log(contaCorrenteRicardo);
-console.log(contaSalario);
+const estalogado = SistemaAutenticacao.login(gerente, "123")
+console.log(estalogado);
